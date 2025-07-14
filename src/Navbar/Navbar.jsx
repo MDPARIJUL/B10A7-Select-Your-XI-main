1c40afcc0d3aca45f32658c90f4f535ad34ed623
MDPARIJUL <md.parijul99@gmail.com>
2025-07-14 17:45:42 +0600

    dropdown menu added

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -9,7 +9,16 @@ const Navbar = () => {
                 <ul className='flex items-center gap-[40px] text-[#131313B3]'>
                     <li>Home</li>
                     <li>Fixture</li>
-                    <li>Teams</li>
+                    <li className='relative group'>Teams
+                        <ul className='absolute top-5 hidden group-hover:block bg-[#e05a3b] px-4 text-center text-[#131313B3]' >
+                            <li className='hover:bg-[#33ff57] mt-2'>Bangladesh </li>
+                            <li className='hover:bg-[#33ff57] mt-2'>India</li>
+                            <li className='hover:bg-[#33ff57] mt-2'>Pakistan</li>
+                            <li className='hover:bg-[#33ff57] mt-2'>Srilanka</li>
+                            <li className='hover:bg-[#33ff57] mt-2'>Nepal</li>
+                        </ul>
+
+                    </li>
                     <li>Schedules</li>
                     <li className='flex items-center gap-4 border-2 border-[#1313131A] rounded-xl px-6 py-4'> <span className='font-semibold'>Coin</span> <button className='text-xl text-[#F39E09] '><BsCoin/></button> </li>
                    
